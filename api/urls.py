07ae09fe704ebfb08d19e706bfdf9e6a26b15489
Saveliy <s.sazono23@gmail.com>
2022-02-01 14:00:31 +0500

    docker

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import upload_image
+from .views import upload_image,test_upload
 
 app_name = 'api'
 
 urlpatterns = [
     path('upload_image', upload_image, name='upload_image'),
-    
+    path('test_upload', test_upload, name='test_upload'),
 ]
