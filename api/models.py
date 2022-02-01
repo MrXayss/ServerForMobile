90993e6ac75d7b40090559ea5c160e97329ff9c1
Saveliy <s.sazono23@gmail.com>
2022-02-01 14:00:31 +0500

    docker

--- api/models.py
+++ api/models.py
@@ -6,6 +6,7 @@ class InfoTrafficLight(models.Model):
     gradus = models.FloatField(max_length=100,verbose_name="Градусы")
     latitude = models.FloatField(max_length=100,verbose_name="Долгота")
     longtitude = models.FloatField(max_length=100,verbose_name="Широта")
+    photo = models.FileField()
     class Meta:
         verbose_name = "Светофор"
         verbose_name_plural = "Список светофоров"
--- api/views.py
+++ api/views.py
@@ -19,3 +19,23 @@ def upload_image(request):
     new_entry = InfoTrafficLight(gradus=z["gradus"], latitude=z["coord1"], longtitude=z["coord2"])    #**z)
     new_entry.save()
     return HttpResponse(request)
+
+@csrf_exempt
+@require_POST
+def test_upload(request):
+    print("Файл: ",request.FILES["record"])
+    print("Градусы: ",request.POST["gradus"])
+    print("Широта: ",request.POST["latitude"])
+    print("Долгота: ",request.POST["longtitude"])
+    new_entry = InfoTrafficLight(gradus=request.POST["gradus"], latitude=request.POST["latitude"], longtitude=request.POST["longtitude"],photo=request.FILES["record"])    #**z)
+    new_entry.save()
+    file_name = str(request.FILES["record"])
+    handle_uploaded_file(
+        request.FILES["record"], file_name
+    )
+    return HttpResponse(request)
+
+def handle_uploaded_file(file, filename):
+    with open("api/uploads/" + filename, "wb+") as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
