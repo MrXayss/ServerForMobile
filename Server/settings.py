8c146cd39830458e19bf24ab15873e478ac62f32
Pavel Zaikin <pzaikin@cloudlinux.com>
2022-02-15 13:29:24 +0500

    fix to docker deploy

--- Server/settings.py
+++ Server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rqh$tw(!6zaqcq$+n)=40czc97q2#hw=q&0yt-q1x%u$m-j527'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.84']
+ALLOWED_HOSTS = ['signal.vita-control.ru', '192.168.1.84']
 
 
 # Application definition
