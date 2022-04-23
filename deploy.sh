0c7d6677af15b25d620903f22a396ae358e43498
Saveliy <s.sazono23@gmail.com>
2022-04-23 21:00:33 +0500

    исправил ошибки

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker build --tag signal:latest .
 docker stop signal-server
 docker rm signal-server
-docker run -v $(pwd)/db:/db -v $(pwd)/media:/media -d --name signal-server --restart unless-stopped --network lan signal:latest
+docker run -v $(pwd)/db:/Server/db -v $(pwd)/media:/Server/media -d --name signal-server --restart unless-stopped --network lan signal:latest
