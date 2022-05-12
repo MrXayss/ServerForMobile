d41b786f7bc18dff6866d84971cde838c78ddea3
Saveliy <s.sazono23@gmail.com>
2022-05-12 14:48:11 +0500

    создание суперюзера

--- entry.sh
+++ entry.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 python manage.py migrate
+echo from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('root', 's.sazono23@gmail.com', 'saw654366') | python manage.py shell
 python manage.py runserver 0.0.0.0:8000 --insecure
